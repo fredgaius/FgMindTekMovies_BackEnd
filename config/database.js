157a927a6a12b6ab4fee92f2dfaf34c1dcb934f8
Fred Gaius OMORUYI <fredgaius@hotmail.com>
2019-10-13 19:44:57 +0200

    Modifying postgreSWL Dababase connectionstring for heroku fix5

--- config/database.js
+++ config/database.js
@@ -1,21 +1,21 @@
 const sequelize = require("sequelize");
 
 // --- Create database connection for sequelize ---
+// FOR LOCALHOST CONFIG
 //-------------------------------------------------
 // Option 1: Passing parameters separately
-module.exports = new sequelize("dvuvcpvwzhxhbn", "dfjdd1qsafimp9", null, {
-    host: process.env.DATABASE_URL,
-    ssl: true,
-    dialect: "postgres",
-    // Optional part. Dont forget adding comma to the end
-    // of line above when you uncommet this bottom part
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-});
+/* module.exports = new sequelize("MovieManagement", "postgres", null, {
+  host: "localhost",
+  dialect: "postgres",
+  // Optional part. Dont forget adding comma to the end
+  // of line above when you uncommet this bottom part
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+}); */
 
 // Option 2: Using a connection URI
 // format is ('postgres://user:pass@example.com:5432/dbname')
@@ -24,3 +24,14 @@ module.exports = new sequelize("dvuvcpvwzhxhbn", "dfjdd1qsafimp9", null, {
 /* module.exports = new sequelize(
   "postgres://postgres:null@localhost:5432/MovieManagement"
 ); */
+
+// FOR HEROKU WEB HOSTING CONFIG
+//-------------------------------------------------
+module.exports = new sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: true
+    }
+});
+
